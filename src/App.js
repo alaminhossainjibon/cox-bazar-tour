0435d3ba89a7ae0286e15da4da890a1136b88065
alaminhossainjibon <mdalamin7622@gmail.com>
2022-04-16 06:04:38 +0600

    background image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
 
+
+
+      <Footer></Footer>
     </div>
   );
 }
